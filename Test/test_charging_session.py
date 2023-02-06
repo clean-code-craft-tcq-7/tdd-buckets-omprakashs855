491874d816e3938b0db0a69bb4046cff6b12a15f
Singh Om Prakash (MS/ESX8-INT-XC) <iom2kor@bosch.com>
2023-02-06 11:59:05 +0530

    (A2D) Step 1: Write KPI and Test, make the test Fail

--- Test/test_charging_session.py
+++ Test/test_charging_session.py
@@ -5,6 +5,7 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 from src.charging_session import ChargingSession as CS
+from src.a2d_convertor import A2D_Convertor as A2D
 
 class TestChargingSession(unittest.TestCase):
 
@@ -12,12 +13,19 @@ class TestChargingSession(unittest.TestCase):
 
         # Creating ChargingSession Class Object
         obj = CS()
+
+        # Creating a Separate A2D Class Object
+        obj_a2d = A2D()
         
         json_data = obj.get_json_data(obj.get_json_path(obj.test_case_json, obj.root_dir))
             
-        for dt in json_data[obj.json_tc]:
-            result = obj.get_reading_count(dt[obj.input])
-            self.assertEqual(result, dt[obj.range])
+        # for dt in json_data[obj.json_tc]:
+        #     result = obj.get_reading_count(dt[obj.input])
+        #     self.assertEqual(result, dt[obj.range])
+        
+        for dt_a2d in json_data[obj_a2d.json_tc_a2d]:
+            result_a2d = obj_a2d.get_current_conv_amp(dt_a2d)
+            self.assertEqual(result_a2d, dt_a2d["Output"])
 
 if __name__ == "__main__":
     unittest.main()
---
+++ src/a2d_convertor.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+import json
+
+class A2D_Convertor:
+
+    def __init__(self):
+        self.json_tc_a2d = "Test Case A2D"
+        self.input = "Input Array"
